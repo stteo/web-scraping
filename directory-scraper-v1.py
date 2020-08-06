ce13cf4193646085f100cd85c4551ee8c4774605
stteo <49391166+stteo@users.noreply.github.com>
2020-08-06 12:58:06 -0700

    Add files via upload

---
+++ directory-scraper-v1.py
@@ -0,0 +1,28 @@
+# This project is to automate web scripting for Siteimprove User Management
+# Last Updated: August 5th, 2020 
+# stteo@ucsc.edu
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys ##to use esc/enter/etc keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# user_list = [stteo@ucsc.edu, tasilva@ucsc.edu, alyehuan@ucsc.edu]
+
+PATH = "/Users/ttwsam/Documents/WebDriver/chromedriver"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://campusdirectory.ucsc.edu/cd_simple") #gets website
+print (driver.title)
+
+search = driver.find_element_by_id("keyword")
+search.send_keys("stteo@ucsc.edu")
+search.send_keys(Keys.RETURN)
+
+
+time.sleep(20)
+driver.quit() 
+
+
