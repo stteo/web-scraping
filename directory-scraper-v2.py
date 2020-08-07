59b03a8744989d9c9c46d35ae3c517f9fd5c1334
Samantha Teo <stteo@ucsc.edu>
2020-08-06 22:52:52 -0700

    fixed search_box.clear() not working

--- directory-scraper-v2.py
+++ directory-scraper-v2.py
@@ -2,24 +2,34 @@
 # Last Updated: August 5th, 2020 
 # stteo@ucsc.edu
 
-# v1 has selenium working to search a user
-
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys ##to use esc/enter/etc keys
+from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+print ("program begins")
+
 PATH = "/Users/ttwsam/Documents/WebDriver/chromedriver"
 driver = webdriver.Chrome(PATH)
-
-driver.get("https://campusdirectory.ucsc.edu/cd_simple") #gets website
+driver.get("https://campusdirectory.ucsc.edu/cd_simple") 
 print (driver.title)
 
-search = driver.find_element_by_id("keyword")
-search.send_keys("stteo@ucsc.edu")
-search.send_keys(Keys.RETURN)
+search_box = driver.find_element_by_id("keyword")
+print ("program is working up to getting website")
 
-time.sleep(20)
+user_list = ["stteo@ucsc.edu", "tasilva@ucsc.edu", "alyehuan@ucsc.edu"]
+
+for user in user_list:
+    print('Siteimprove User ID (' + user + ') testing')
+    search_box.send_keys(user)
+    search_box.send_keys(Keys.RETURN)
+
+    search_box = driver.find_element_by_id("keyword") #have to redeclare the element again for some reason 
+    search_box.clear()
+    
+time.sleep(10)
+print ("end of program")
 driver.quit() 
+
