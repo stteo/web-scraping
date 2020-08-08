d5977a2824f2f149c960026683b171c636fc2d1d
Samantha Teo <stteo@ucsc.edu>
2020-08-07 23:34:13 -0700

    v3 uses csv to iterate through emails

--- directory-scraper-v3.py
+++ directory-scraper-v3.py
@@ -1,7 +1,7 @@
-# Version 2 can successfully use Selenium by iterating through a list. 
+# Version 3 can successfully use import a csv file and iterate through the email row with Selenium
 
 # This project is to automate web scripting for Siteimprove User Management
-# Last Updated: August 5th, 2020 
+# Last Updated: August 7th, 2020 
 # stteo@ucsc.edu
 
 from selenium import webdriver
@@ -10,27 +10,30 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import pandas as pd
 
-print ("program begins")
-
+# SETUP 
+print ("program begins and setup starts")
+df = pd.read_csv('users-list.csv')
 PATH = "/Users/ttwsam/Documents/WebDriver/chromedriver"
 driver = webdriver.Chrome(PATH)
+print (driver.title + "setup complete")
+
 driver.get("https://campusdirectory.ucsc.edu/cd_simple") 
-print (driver.title)
-
 search_box = driver.find_element_by_id("keyword")
-print ("program is working up to getting website")
+print ("got website and search box")
 
-user_list = ["stteo@ucsc.edu", "tasilva@ucsc.edu", "alyehuan@ucsc.edu"]
-
-for user in user_list:
-    print('Siteimprove User ID (' + user + ') testing')
-    search_box.send_keys(user)
+# ITERATE THROUGH LIST
+for index, row in df.iterrows():
+    current_user = row['Email']
+    print('Testing for Siteimprove User ID (' + current_user + ')')
+    search_box.send_keys(current_user)
     search_box.send_keys(Keys.RETURN)
 
     search_box = driver.find_element_by_id("keyword") #have to redeclare the element again for some reason 
     search_box.clear()
-    
-time.sleep(10)
+
+# BREAKDOWN 
+time.sleep(5)
 print ("end of program")
 driver.quit() 
