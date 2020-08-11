9442b62e68242252913d4009b4eae08c6b65e7f4
Samantha Teo <stteo@ucsc.edu>
2020-08-11 12:08:02 -0700

    directory-scraper-v4.py base functionality working
    Version 4 uses campus directory advanced and catches edge cases where 1+ users are found with campus email search

--- directory-scraper-v4.py
+++ directory-scraper-v4.py
@@ -1,4 +1,4 @@
-# Version 3 can successfully use import a csv file and iterate through the email row with Selenium
+# Version 4 uses campus directory advanced and catches edge cases where 1+ users are found with campus email search
 
 # This project is to automate web scripting for Siteimprove User Management
 # Last Updated: August 7th, 2020 
@@ -14,30 +14,57 @@ import pandas as pd
 
 # SETUP 
 print ("directory-scraper-v4.py - program starts")
-df = pd.read_csv('users-list.csv')
+df = pd.read_csv('users-list-testing.csv')
 PATH = "/Users/ttwsam/Documents/WebDriver/chromedriver"
 driver = webdriver.Chrome(PATH)
 print (driver.title + "setup complete")
 
+print(df[['Name', 'Email']])
+
 driver.get("https://campusdirectory.ucsc.edu/cd_advanced") 
-search_box = driver.find_element_by_name("data[mail][0]")
+email_search_box = driver.find_element_by_name("data[mail][0]")
+
+correct_message = ("1 record matched your search request.")
+incorrect_message = ("Your search did not return any records")
+index_count = 0
+print("the first index count is " + df["Name"].iloc[index_count])
 
 # ITERATE THROUGH LIST
-for index, row in df.iterrows():s
+for index, row in df.iterrows():
     current_user = row['Email']
-    search_box.send_keys(current_user)
-    search_box.send_keys(Keys.RETURN)
+    email_search_box.send_keys(current_user)
+    email_search_box.send_keys(Keys.RETURN)
 
-    element = WebDriverWait(driver, 3).until(
+    element = WebDriverWait(driver, 4).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="directoryContent"]/h3[2]'))
     )
-    
-    print('Name: (' + current_user + '): ' + element.text)
-   
-    search_box = driver.find_element_by_id("data[mail][0]") #have to redeclare the element again for some reason 
-    search_box.clear()
+
+    if (element.text) == (correct_message):
+        print('Siteimprove User (' + current_user + '): ' + element.text)
+    elif (element.text) == (incorrect_message):
+        print('Siteimprove User (' + current_user + '): ' + element.text)
+    else: 
+        #name_search_box = driver.find_element_by_name("data[cn][0]")
+        print('Siteimprove User (' + current_user + '): ' + element.text)
+        name_search_box = driver.find_element_by_name("data[cn][0]")
+        name_search_box.send_keys(df["Name"].iloc[index_count])
+        name_search_box.send_keys(Keys.RETURN)
+        element_name_search = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="directoryContent"]/h3[2]'))
+        )
+        print("     Searching for " + df["Name"].iloc[index_count] + " returns: " + element_name_search.text)
+
+
+    index_count = (index_count + 1) 
+
+    email_search_box = driver.find_element_by_name("data[mail][0]") #have to redeclare the element again for some reason 
+    email_search_box.clear()
+    name_search_box = driver.find_element_by_name("data[cn][0]") #have to redeclare the element again for some reason 
+    name_search_box.clear()
 
 # BREAKDOWN 
 time.sleep(5)
 print ("directory-scraper-v4.py - program ends")
 driver.quit() 
+
+# finding href id by ## //*[@id="dresults"]/tbody/tr[2]/td[1]/a
