83c1ac1a1f82ff8f801d7c0a835fbc7068edce1c
Samantha Teo <stteo@ucsc.edu>
2020-08-13 16:37:45 -0700

    added comments and edits for readability

--- directory-scraper-v4.py
+++ directory-scraper-v4.py
@@ -14,54 +14,59 @@ import pandas as pd
 
 # SETUP 
 print ("directory-scraper-v4.py - program starts")
-df = pd.read_csv('users-list.csv')
+df = pd.read_csv('users-list-testing.csv') # reads csv into pandas dataframe
 PATH = "/Users/ttwsam/Documents/WebDriver/chromedriver"
 driver = webdriver.Chrome(PATH)
 print (driver.title + "setup complete")
 
-driver.get("https://campusdirectory.ucsc.edu/cd_advanced") 
-email_search_box = driver.find_element_by_name("data[mail][0]")
+driver.get("https://campusdirectory.ucsc.edu/cd_advanced") # open campus directory advanced
+email_search_box = driver.find_element_by_name("data[mail][0]") # find "Campus Email" search box 
 
 correct_message = ("1 record matched your search request.")
 incorrect_message = ("Your search did not return any records")
 index_count = 0
 
-# ITERATE THROUGH LIST
+# CHECK USERS AGAINST CAMPUS DIRECTORY
 for index, row in df.iterrows():
+
+    # input current_user into "Campus Email" search box
     current_user = row['Email']
     email_search_box.send_keys(current_user)
     email_search_box.send_keys(Keys.RETURN)
 
+    # finds search results and sets to var "element"  
     element = WebDriverWait(driver, 4).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="directoryContent"]/h3[2]'))
     )
 
+    # print out if user has been found or not
     if (element.text) == (correct_message):
         print('Siteimprove User (' + current_user + '): ' + element.text)
-        
     elif (element.text) == (incorrect_message):
         print('Siteimprove User (' + current_user + '): ' + element.text)
-    else: 
-        #name_search_box = driver.find_element_by_name("data[cn][0]")
+    else: # if 2+ users are found when searching "Campus Email", search again for "Full Name"
+        name_search_box = driver.find_element_by_name("data[cn][0]") # find "Full Name" search box
         print('Siteimprove User (' + current_user + '): ' + element.text)
-        name_search_box = driver.find_element_by_name("data[cn][0]")
         name_search_box.send_keys(df["Name"].iloc[index_count])
         name_search_box.send_keys(Keys.RETURN)
         element_name_search = WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="directoryContent"]/h3[2]'))
         )
         print("     Searching for " + df["Name"].iloc[index_count] + " returns: " + element_name_search.text)
+        
+        # clear "Full Name" search box 
+        name_search_box = driver.find_element_by_name("data[cn][0]") 
+        name_search_box.clear()
 
-    index_count = (index_count + 1) 
-
-    email_search_box = driver.find_element_by_name("data[mail][0]") #have to redeclare the element again for some reason 
+    # clear "Campus Email" search box 
+    email_search_box = driver.find_element_by_name("data[mail][0]") 
     email_search_box.clear()
-    name_search_box = driver.find_element_by_name("data[cn][0]") #have to redeclare the element again for some reason 
-    name_search_box.clear()
+    
+    # advance index_count 
+    index_count = (index_count + 1) 
 
 # BREAKDOWN 
 time.sleep(5)
 print ("directory-scraper-v4.py - program ends")
 driver.quit() 
 
-# finding href id by ## //*[@id="dresults"]/tbody/tr[2]/td[1]/a
--- directory-scraper-v5.py
+++ directory-scraper-v5.py
@@ -1,4 +1,4 @@
-# Version 4 uses campus directory advanced and catches edge cases where 1+ users are found with campus email search
+# Version 5 gets data from Siteimprove API 
 
 # This project is to automate web scripting for Siteimprove User Management
 # Last Updated: August 7th, 2020 
@@ -14,9 +14,9 @@ import pandas as pd
 import requests
 import json 
 
-print ("directory-scraper-v4.py - program starts")
+print ("directory-scraper-v5.py - program starts")
 
-# SETUP SITEIMPROVE API KEY 
+# SETUP SITEIMPROVE API 
 api_user = "stteo@ucsc.edu"
 api_key = "2d907394ada437a4161b83100d97ad85"
 resource = "https://api.siteimprove.com/v2"
@@ -34,32 +34,34 @@ PATH = "/Users/ttwsam/Documents/WebDriver/chromedriver"
 driver = webdriver.Chrome(PATH)
 print (driver.title + "setup complete")
 
-driver.get("https://campusdirectory.ucsc.edu/cd_advanced") # navigates to campus directory link
-email_search_box = driver.find_element_by_name("data[mail][0]") # searches for "campus email" search box by html element name
+driver.get("https://campusdirectory.ucsc.edu/cd_advanced") # navigates to campus directory advanced search
+email_search_box = driver.find_element_by_name("data[mail][0]") # searches for "campus email" search box by HTML element name
 
-# SETUP DATA SOURCE (currently using api)
-# df = pd.read_csv('users-list.csv')
-df = pd.DataFrame(account["items"])
+# SETUP DATA + SEARCH RESULT VERIFICATION
+df = pd.DataFrame(account["items"]) # reads json pulled from Siteimprove API to Pandas dataframe
 correct_message = ("1 record matched your search request.")
 incorrect_message = ("Your search did not return any records")
-index_count = 0 
+index_count = 0 # init counter for current_user position
 
 # CHECK USERS AGAINST CAMPUS DIRECTORY
 for index, row in df.iterrows():
+
+    # input current_user into "Campus Email" search box
     current_user = row['email']
     email_search_box.send_keys(current_user)
     email_search_box.send_keys(Keys.RETURN)
 
+    # finds search results and sets to var "element"  
     element = WebDriverWait(driver, 4).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="directoryContent"]/h3[2]'))
     )
 
+    # print out if user has been found or not
     if (element.text) == (correct_message):
         print('Siteimprove User (' + current_user + '): ' + element.text)
-        
     elif (element.text) == (incorrect_message):
         print('Siteimprove User (' + current_user + '): ' + element.text)
-    else: 
+    else: # if 2+ users are found when searching "Campus Email", search again for "Full Name"
         print('Siteimprove User (' + current_user + '): ' + element.text)
         name_search_box = driver.find_element_by_name("data[cn][0]")
         name_search_box.send_keys(df["name"].iloc[index_count])
@@ -68,15 +70,19 @@ for index, row in df.iterrows():
             EC.presence_of_element_located((By.XPATH, '//*[@id="directoryContent"]/h3[2]'))
         )
         print("     Searching for " + df["name"].iloc[index_count] + " returns: " + element_name_search.text)
+        
+        # clear "Full Name" search box 
+        name_search_box = driver.find_element_by_name("data[cn][0]") 
+        name_search_box.clear()
 
-    index_count = (index_count + 1) 
-
+    # clear "Campus Email" search box 
     email_search_box = driver.find_element_by_name("data[mail][0]") 
     email_search_box.clear()
-    name_search_box = driver.find_element_by_name("data[cn][0]") 
-    name_search_box.clear()
+    
+    # advance index_count 
+    index_count = (index_count + 1) 
 
 # BREAKDOWN 
 time.sleep(5)
-print ("directory-scraper-v4.py - program ends")
+print ("directory-scraper-v5.py - program ends")
 driver.quit() 
